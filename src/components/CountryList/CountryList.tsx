fc91a5a5994ee885ae3c77f4b5b8cc392d718cd7
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-20 19:34:09 +0600

    refactor CountryList; deleted logic; stayed just component;

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,33 +1,18 @@
+import React from 'react';
 import {List} from '@mui/material';
 import CountryItem from './CountryItem/CountryItem';
-import {useCallback, useEffect, useState} from 'react';
-import {ApiCountry, CountryItemType} from '../../types';
-import axios from 'axios';
-import {NAMES_URL} from '../../constans';
+import {CountryItemType} from '../../types';
 
-const CountryList = () => {
-  const [countries, setCountries] = useState<CountryItemType[]>([]);
-
-  const fetchCountries = useCallback(async () => {
-    const { data: countries } = await axios.get<ApiCountry[]>(NAMES_URL);
-    console.log(countries);
-
-    const allCountries = countries.map(country => ({
-      name: country.name,
-    }));
-
-    setCountries(allCountries);
-  }, []);
-
-  useEffect(() => {
-    void fetchCountries();
-  }, [fetchCountries]);
-
+interface CountryListProps {
+  countries: CountryItemType[];
+  getCountryInfo: (country: CountryItemType) => void;
+}
+const CountryList: React.FC<CountryListProps> = ({countries, getCountryInfo}) => {
 
   return (
     <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
       {countries.map((country, index) => (
-        <CountryItem key={index} name={country.name} />
+        <CountryItem key={index} name={country.name} onClick={() => getCountryInfo(country)}/>
       ))}
     </List>
   );

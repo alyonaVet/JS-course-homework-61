4d95d003c57ee35bdd59a534772f3e736bc90753
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-20 14:53:53 +0600

    added axios.defaults.baseURL;

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://restcountries.com/v2/';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>

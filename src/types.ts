90e98bf6e061a5e28c3e1cd52a561d2b382d66c7
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-20 20:17:25 +0600

    added CountryInfoType, ApiCountryInfo;

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,47 @@
 export interface CountryItemType {
   name: string;
+  alpha3Code: string;
+}
+
+export interface CountryInfoType {
+  name: string;
+  capital: string;
+  population: number;
+  borders: Array<string>;
+  flag: string;
 }
 
 export interface ApiCountry {
   name: string;
   alpha3Code: string;
   independent: boolean;
+}
+
+export interface ApiCountryInfo {
+  "name": string;
+  "topLevelDomain": Array<string>;
+  "alpha2Code": string;
+  "alpha3Code": string;
+  "callingCodes": Array<string>;
+  "capital": string;
+  "altSpellings": Array<string>;
+  "subregion": string;
+  "region": string;
+  "population": number;
+  "latlng": Array<string>;
+  "demonym": string;
+  "area": number;
+  "gini": number;
+  "timezones": Array<string>;
+  "borders": Array<string>;
+  "nativeName": string;
+  "numericCode": string;
+  "flags": Record<string, string>;
+  "currencies": Array<string>;
+  "languages": Array<string>;
+  "translations": Record<string, string>;
+  "flag": string;
+  "regionalBlocs": Array<string>;
+  "cioc": string;
+  "independent": boolean;
 }

d72a932e6175fc6568cf68ba8947f323cdaa5acb
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-21 14:21:28 +0600

    added neighbours state, setNeighbours;

--- src/containers/CountriesBox/CountriesBox.tsx
+++ src/containers/CountriesBox/CountriesBox.tsx
@@ -1,10 +1,11 @@
 import {Box, Stack} from '@mui/material';
 import CountryList from '../../components/CountryList/CountryList';
 import {useCallback, useEffect, useState} from 'react';
-import {ApiCountry, ApiCountryInfo, CountryInfoType, CountryItemType} from '../../types';
+import {ApiCountry, ApiCountryInfo, CountryInfoType, CountryItemType, NeighbourType} from '../../types';
 import axios from 'axios';
 import {NAME_URL, NAMES_URL} from '../../constans';
 import CountryInfo from '../../components/CountryInfo/CountryInfo';
+
 
 const CountriesBox = () => {
 
@@ -16,8 +17,11 @@ const CountriesBox = () => {
       population: 0,
       borders: [''],
       flag: '',
+      region: ''
     }
   );
+  const [neighbours, setNeighbours] = useState<NeighbourType[]>([]);
+
 
   const fetchCountries = useCallback(async () => {
     const {data: countries} = await axios.get<ApiCountry[]>(NAMES_URL);
@@ -37,11 +41,21 @@ const CountriesBox = () => {
       name: country.name,
       capital: country.capital,
       population: country.population,
-      borders: country.borders ? country.borders : [],
+      borders: country.borders || [],
       flag: country.flag,
+      region: country.region,
     };
-
     setCountry(countryInfo);
+
+    if (countryInfo.borders.length > 0) {
+      const neighbourPromises = countryInfo.borders.map(border =>
+        axios.get<NeighbourType>(NAME_URL + border)
+      );
+      const neighboursData = await Promise.all(neighbourPromises);
+      setNeighbours(neighboursData.map(neighbour => neighbour.data));
+    } else {
+      setNeighbours([]);
+    }
   }, []);
 
 
@@ -50,8 +64,7 @@ const CountriesBox = () => {
   }, [fetchCountries]);
 
   const getCountryInfo = async (country: CountryItemType) => {
-    console.log(`Getting country info of ${country.name}`);
-    void await fetchOneCountry(country.alpha3Code);
+    await fetchOneCountry(country.alpha3Code);
   };
 
   return (
@@ -59,7 +72,7 @@ const CountriesBox = () => {
       <Box
         sx={{
           width: '100%',
-          height: 500,
+          height: 550,
           maxWidth: 360,
           bgcolor: 'background.paper',
           overflowY: 'auto',
@@ -68,8 +81,7 @@ const CountriesBox = () => {
         }}>
         <CountryList countries={countries} getCountryInfo={getCountryInfo}/>
       </Box>
-      <CountryInfo country={country}/>
-
+      <CountryInfo country={country} neighbours={neighbours}/>
     </Stack>
 
 

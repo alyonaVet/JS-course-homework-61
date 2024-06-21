68d585c2450fb60760cf4b614a29674e195492bc
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-21 13:35:13 +0600

    refactor countryInfo; added neighbours; added if no country checked;

--- src/components/CountryInfo/CountryInfo.tsx
+++ src/components/CountryInfo/CountryInfo.tsx
@@ -1,20 +1,37 @@
-import {Box, Container, Stack, Typography} from '@mui/material';
+import {Box, Container, List, ListItem, ListItemText, Stack, Typography} from '@mui/material';
 import React from 'react';
-import {CountryInfoType} from '../../types';
+import {CountryInfoType, NeighbourType} from '../../types';
 
 interface CountryInfoProps {
   country: CountryInfoType;
+  neighbours: NeighbourType[];
 }
 
+const CountryInfo: React.FC<CountryInfoProps> = ({country, neighbours}) => {
 
-const CountryInfo: React.FC<CountryInfoProps> = ({country}) => {
-
+  if (!country || !country.name) {
+    return (
+      <Container
+        sx={{
+          width: '100%',
+          height: 'auto',
+          bgcolor: 'background.paper',
+          border: '1px solid lightgrey',
+          borderRadius: 1,
+          padding: '1rem',
+        }}
+      >
+        <Typography variant="h5" align="center">Select a country to get information about it!</Typography>
+      </Container>
+    );
+  }
   return (
     <Container
       sx={{
         width: '100%',
-        height: 500,
+        height: 'auto',
         bgcolor: 'background.paper',
+        overflowY: 'auto',
         border: '1px solid lightgrey',
         borderRadius: 1,
         padding: '1rem',
@@ -24,6 +41,7 @@ const CountryInfo: React.FC<CountryInfoProps> = ({country}) => {
           <Typography variant="h4" component="h4" sx={{marginBottom: '1.5rem'}}>{country.name}</Typography>
           <Typography variant="h6" component="p">Capital: {country.capital}</Typography>
           <Typography variant="h6" component="p">Population: {country.population} people</Typography>
+          <Typography variant="h6" component="p">Region: {country.region}</Typography>
         </Box>
         <Box
           component="img"
@@ -34,13 +52,21 @@ const CountryInfo: React.FC<CountryInfoProps> = ({country}) => {
       </Stack>
 
       <Box>
-        <Typography variant="h5" component="p">Borders with:</Typography>
-        {country.borders.map((border, index) => (
-          <Typography key={index} variant="h6" component="p">{border}</Typography>
-        ))}
-
+        <Typography variant="h5" component="p" marginTop={2}>Borders with:</Typography>
+        <List sx={{marginLeft: '100px'}}>
+          {neighbours.length > 0 && (
+            neighbours.map((neighbour, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={neighbour.name}/>
+              </ListItem>
+            ))
+          )}
+          {neighbours.length === 0 && (
+            <ListItem>
+              <ListItemText primary={'No borders'}/>
+            </ListItem>)}
+        </List>
       </Box>
-
     </Container>
   );
 };
